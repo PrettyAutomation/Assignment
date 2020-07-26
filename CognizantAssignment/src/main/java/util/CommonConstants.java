43bc51b1a4b69219c2b1b4c9500bde10785c2fea
pretty.sanwale <pretty.sanwale@servicenow.com>
2020-07-26 11:48:47 +0530

    Test:Cognizant assignment completed

---
+++ CognizantAssignment/src/main/java/util/CommonConstants.java
@@ -0,0 +1,31 @@
+package util;
+
+public interface CommonConstants {
+
+    String login = "//a[text() = 'Login']";
+    String searchBox = "//input[@title='Search for products, brands and more']";
+    String submit = "//button[@type='submit']";
+    String close = "//button[@class='_2AkmmA _29YdH8']";
+    String listOfCameraResult = "//img[@class='_1Nyybr _30XEf0']";
+    String listOfPrice = "//div[@class = '_1vC4OE _2rQ-NK']";
+    String CartPageNameXpath = "//span[@class='_35KyD6']";
+    String CartPagePriceXpath = "//div[@class= '_1vC4OE _3qQ9m1']";
+    String BuyNow = "//button[@type = 'button']";
+    String EnterMobileNo = "//input[@type = 'text']";
+    String ErrorOnWrongMobileOrEmail = "//span[text()='Please enter valid Email ID/Mobile number']";
+    String Continue = "//span[text() = 'CONTINUE']";
+    String EnterPassword = "//input[@type='password']";
+    String WrongUserNamePsw = "//span[text() = 'Your username or password is incorrect']";
+    String MyAccount = "//div[text() = 'My Account']";
+    String svgForLogout = "//*[local-name() ='svg']/*[local-name()='path'][1]";
+    String Logout = "//span[text()='Logout']";
+
+
+
+    String searchItem = "searchitem";
+    String perticularCameraItem = "perticularCameraItem";
+    String ExcelPath = "src/main/resources/Data_v0.1.xlsx";
+
+
+
+}
